26883c935fcaae3d58a239c66f728b083d1d0b9d
jfornielestiburcio02-web <jfornielestiburcio02@gmail.com>
2026-05-06 15:33:32 +0200

    Update CEC.php

--- api/CEC.php
+++ api/CEC.php
@@ -53,74 +53,54 @@ if (!$tienePermiso) {
 $rolMin = strtolower($rolSolicitado);
 ?>
 
-<!DOCTYPE html>
-<html lang="es">
-<head>
+
+
+<HTML>
+
+    
     <meta charset="UTF-8">
-    <title>Panel Operativo - 061 Málaga</title>
-    <style>
-        /* Estilos de estructura limpia */
-        body, html {
-            margin: 0; padding: 0;
-            height: 100%; width: 100%;
-            overflow: hidden;
-            font-family: Verdana, sans-serif;
-            background-color: #fff;
-        }
+<TITLE>CEC</TITLE>
+    
+    <link rel="stylesheet" href="/css/CEC.css"
 
-        /* Cabecera */
-        #frame-header {
-            position: absolute;
-            top: 0; left: 0;
-            width: 100%; height: 60px;
-            z-index: 100;
-            border-bottom: 1px solid #C5E1D1;
-        }
 
-        /* Sidebar con efecto Hover */
-        #contenedor-sidebar {
-            position: absolute;
-            top: 60px; left: 0; bottom: 0;
-            width: 50px; /* Cerrado */
-            z-index: 50;
-            background: #fff;
-            border-right: 1px solid #C5E1D1;
-            transition: width 0.3s ease;
-            overflow: hidden;
-        }
 
-        #contenedor-sidebar:hover {
-            width: 200px; /* Abierto */
-            box-shadow: 5px 0 15px rgba(0,0,0,0.1);
-        }
-
-        #frame-sidebar {
-            width: 200px; height: 100%; border: 0;
-        }
-
-        /* Contenido */
-        #frame-content {
-            position: absolute;
-            top: 60px; left: 50px;
-            right: 0; bottom: 0;
-            width: calc(100% - 50px);
-            height: calc(100% - 60px);
-            border: 0;
-        }
-    </style>
+    
 </head>
-<body>
 
-    <!-- Header -->
-    <iframe id="frame-header" src="header/<?php echo $rolMin; ?>/prin.php" name="header"></iframe>
 
-    <!-- Sidebar -->
+
+
+    
+        <body>
+
+
+    
+
+    <iframe id="frame-header" src="/header/<?php echo $rolMin; ?>/prin.php" name="header"></iframe>
+
+
+
+
+    
     <div id="contenedor-sidebar">
-        <iframe id="frame-sidebar" src="sidebar/<?php echo $rolMin; ?>/prin.php" name="sidebar"></iframe>
+        <iframe id="frame-sidebar" src="/sidebar/<?php echo $rolMin; ?>/prin.php" name="sidebar"></iframe>
     </div>
 
-    <!-- Contenido -->
-    <iframe id="frame-content" src="content/<?php echo $rolMin; ?>/prin.php" name="content"></iframe>
+
+    
+    
+    <iframe id="frame-content" src="/contenidoEmpleado/<?php echo $rolMin; ?>/prin.php" name="content"></iframe>
 
 </body>
-</html>
+
+
+
+
+
+
+
+
+
+    
+</HTML>
